82c7e552753d8ad9b414af8ad41a79f84fb55369
Johan Denoyer <johan@johandenoyer.fr>
2013-07-18 17:56:31 +0200

    add mysql backup script
    Backup using a file for each database.
    Locks all databases during backup

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Different mysql tools
 
 - optimizealltables.sh		=> Optimise all tables of all databases
 - repairalltables.sh		=> Repair all tables of all databases
+- backup.sh					=> Backup mysql databases to different sql file. Databases are locked during backup.
---
+++ backup.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+ 
+TIMESTAMP=$(date +"%F")
+BACKUP_DIR="/backup/$TIMESTAMP"
+MYSQL_USER="backup"
+MYSQL_PASSWORD="password"
+MYSQL=/usr/bin/mysql
+MYSQLDUMP=/usr/bin/mysqldump
+ 
+mkdir -p $BACKUP_DIR
+
+# Lock database
+$MYSQL --user=$MYSQL_USER -p$MYSQL_PASSWORD -e "FLUSH TABLES WITH READ LOCK;SET GLOBAL read_only = ON;"
+
+#Backup database
+databases=`$MYSQL --user=$MYSQL_USER -p$MYSQL_PASSWORD -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema)"`
+ 
+for db in $databases; do
+  $MYSQLDUMP --force --opt --user=$MYSQL_USER -p$MYSQL_PASSWORD --databases $db | gzip > "$BACKUP_DIR/$db.gz"
+done
+#remove lock
+$MYSQL --user=$MYSQL_USER -p$MYSQL_PASSWORD -e "SET GLOBAL read_only = OFF;UNLOCK TABLES;"
